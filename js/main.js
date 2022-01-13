ec2b369d537b7bad34633cefc2d151d3da0c25dc
AniKal25 <kal21006@byui.edu>
2022-01-13 08:49:31 -0800

    Week 1

---
+++ js/main.js
@@ -0,0 +1,30 @@
+const links = [
+    {
+      label: "Week 1 Notes",
+      url: "week1/index.html"
+      
+    },
+
+    {
+      label: "Week 2 Notes",
+      url: "week2/index.html"
+      
+    }
+  ]
+
+  let ol = document.getElementById("linksList");
+
+
+
+for (let i=0; i < links.length; i++) {
+    console.log(links)
+    let li = document.createElement("li");
+    let a = document.createElement("a");
+
+    a.textContent = links[i].label;
+    a.setAttribute ("href", links[i].url)
+
+    li.append(a);
+    ol.append(li);
+   
+};
